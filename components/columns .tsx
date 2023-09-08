66954d10fdc4721835fc42b8c6092a3f9a210be6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-09-08 15:07:55 -0600

    feat: add new columns and update status column

--- components/columns .tsx
+++ components/columns .tsx
@@ -3,8 +3,6 @@
 import { ColumnDef } from "@tanstack/react-table"
 
 import { Badge } from "./ui/badge"
-import { Checkbox } from "./ui/checkbox"
-
 import { labels, priorities, statuses } from "../data/data"
 import { Task } from "../data/schema"
 import { DataTableColumnHeader } from "./data-table-column-header"
@@ -12,45 +10,15 @@ import { DataTableRowActions } from "./data-table-row-actions"
 
 export const columns: ColumnDef<Task>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected()}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-[2px]"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-[2px]"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "id",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Task" />
-    ),
-    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
     accessorKey: "title",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Title" />
+      <DataTableColumnHeader className="ml-4" column={column} title="Title" />
     ),
     cell: ({ row }) => {
       const label = labels.find((label) => label.value === row.original.label)
 
       return (
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 ml-2">
           {label && <Badge variant="outline">{label.label}</Badge>}
           <span className="max-w-[500px] truncate font-medium">
             {row.getValue("title")}
@@ -75,9 +43,9 @@ export const columns: ColumnDef<Task>[] = [
 
       return (
         <div className="flex w-[100px] items-center">
-          {status.icon && (
-            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
+          <span className="mr-2 h-4 w-4">
+            {status.icon}
+          </span>
           <span>{status.label}</span>
         </div>
       )
@@ -111,6 +79,54 @@ export const columns: ColumnDef<Task>[] = [
     },
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id))
+    },
+  },
+  {
+    accessorKey: "lastReview",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Last Review" />
+    ),
+    cell: ({ row }) => {
+
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue("lastReview")}
+          </span>
+        </div>
+      )
+    },
+  },
+  {
+    accessorKey: "nextReview",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Next Review" />
+    ),
+    cell: ({ row }) => {
+      
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue("nextReview")}
+          </span>
+        </div>
+      )
+    },
+  },
+  {
+    accessorKey: "reviewCount",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Review Count" />
+    ),
+    cell: ({ row }) => {
+      
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue("reviewCount")}
+          </span>
+        </div>
+      )
     },
   },
   {
