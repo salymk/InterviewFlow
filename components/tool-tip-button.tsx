9e3096fc8147ee22dceace8a12723ecc30d6b017
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-09-09 11:33:31 -0600

    fix: allow ToolTipButton to be used as a child component

--- components/tool-tip-button.tsx
+++ components/tool-tip-button.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Button } from "./ui/button"
 import {
   Tooltip,
@@ -6,22 +7,24 @@ import {
   TooltipTrigger,
 } from "./ui/tooltip"
 
-interface ToolTipButtonProps {
-  buttonText: string
-  tooltipText: string
-}
 
-export function ToolTipButton({buttonText, tooltipText}: ToolTipButtonProps) {
+function ToolTipButton(props, forwardRef) {
   return (
     <TooltipProvider>
       <Tooltip>
+        {/* The asChild prop allows you to apply the behaviors and functionalities of a Radix primitive to your custom element or component. 
+        Essentially, when asChild is set to true, Radix does not render its default element, 
+        but instead, it applies its behaviors to the child element you provide. i.e. Button has the functionality of TooltipTrigger and 
+        SheetTrigger since ToolTipButton is its child. */}
         <TooltipTrigger asChild>
-          <Button className="h-8 hover:bg-slate-200" variant="outline">{buttonText}</Button>
+          <Button {...props} ref={forwardRef} className="h-8 hover:bg-slate-200" variant="outline">{props.buttonText}</Button>
         </TooltipTrigger>
         <TooltipContent>
-          <p>{tooltipText}</p>
+          <p>{props.tooltipText}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
   )
 }
+
+export default React.forwardRef(ToolTipButton)
