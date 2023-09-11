2d7d78dea9bc6a7a4a185c8b50c6caf3020a3c15
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-09-11 16:34:09 -0600

    remove ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,41 @@
-// import Image from 'next/image'
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
 
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <h1 className="text-4xl font-bold text-center">Interview Flow</h1>
-//     </main>
-//   )
-// }
-
-import { promises as fs } from "fs"
-import path from "path"
-import { Metadata } from "next"
-import Image from "next/image"
-import { z } from "zod"
-
-import { columns } from '../components/columns '
-import { DataTable } from "../components/data-table"
-import { UserNav } from "../components/user-nav"
-import { taskSchema } from "../data/schema"
+import { columns } from "../components/columns ";
+import { DataTable } from "../components/data-table";
+import { UserNav } from "../components/user-nav";
+import { taskSchema } from "../data/schema";
 
 export const metadata: Metadata = {
   title: "InterviewFlow",
-  description: "Spaced repetition + technical interview prep will help you flow right into your next interview.",
-}
+  description:
+    "Spaced repetition + technical interview prep will help you flow right into your next interview.",
+};
 
 // Simulate a database read for tasks.
 async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), "data/tasks.json")
-  )
+  const data = await fs.readFile(path.join(process.cwd(), "data/tasks.json"));
 
-  const tasks = JSON.parse(data.toString())
+  const tasks = JSON.parse(data.toString());
 
-  return z.array(taskSchema).parse(tasks)
+  return z.array(taskSchema).parse(tasks);
 }
 
 export default async function Home() {
-  const tasks = await getTasks()
+  const tasks = await getTasks();
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/tasks-light.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/tasks-dark.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">InterviewFlow</h2>
             <p className="text-muted-foreground">
-            Spaced repetition + technical interview prep will help you flow right into your next interview.
+              Spaced repetition + technical interview prep will help you flow
+              right into your next interview.
             </p>
           </div>
           <div className="flex items-center space-x-2">
@@ -71,5 +45,5 @@ export default async function Home() {
         <DataTable data={tasks} columns={columns} />
       </div>
     </>
-  )
+  );
 }
