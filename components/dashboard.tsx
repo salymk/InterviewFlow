c4fa0039afa768d83c9578fe0227079fb6a19186
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-09-23 17:00:53 -0600

    feat: toggle button to expand sidebar

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -12,6 +12,8 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { UserNav } from "./user-nav";
+import { ChevronRightIcon, ChevronLeftIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
@@ -27,12 +29,14 @@ const teams = [
   { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
 ];
 
+// A utility for conditionally applying class names based on certain conditions or states
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Dashboard({ children }: { children: any }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [expanded, setExpanded] = useState(true);
 
   return (
     <>
@@ -170,9 +174,24 @@ export default function Dashboard({ children }: { children: any }) {
         </Transition.Root>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div
+          className={classNames(
+            expanded ? "lg:w-72" : "lg:w-20",
+            "hidden relative transition-all lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col"
+          )}
+        >
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+          <div
+            className={`flex grow flex-col gap-y-5 lg:z-51 overflow-y-auto border-r border-gray-200  bg-white px-6`}
+          >
+            <Button
+              variant="secondary"
+              size="icon"
+              className="rounded-full absolute top-[35px] right-[-35px] transform -translate-x-1/2 hover:bg-slate-950 hover:text-white"
+              onClick={() => setExpanded(!expanded)}
+            >
+              {expanded ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </Button>
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
@@ -189,59 +208,38 @@ export default function Dashboard({ children }: { children: any }) {
                         <a
                           href={item.href}
                           className={classNames(
+                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
                             item.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                              ? "bg-slate-950 text-slate-100"
+                              : "text-slate-950",
+                            expanded ? "ml-0" : "ml-1",
+                            !item.current &&
+                              "hover:text-slate-100 hover:bg-slate-950"
                           )}
                         >
                           <item.icon
                             className={classNames(
                               item.current
-                                ? "text-indigo-600"
-                                : "text-gray-400 group-hover:text-indigo-600",
+                                ? "text-slate-100"
+                                : "text-slate-950 group-hover:text-slate-100",
                               "h-6 w-6 shrink-0"
                             )}
                             aria-hidden="true"
                           />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
-                        >
+
                           <span
-                            className={classNames(
-                              team.current
-                                ? "text-indigo-600 border-indigo-600"
-                                : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                            )}
+                            className={`overflow-hidden transition-all ${
+                              expanded ? "w-52" : "w-0"
+                            }`}
                           >
-                            {team.initial}
+                            {item.name}
                           </span>
-                          <span className="truncate">{team.name}</span>
                         </a>
                       </li>
                     ))}
                   </ul>
                 </li>
+
                 <li className="-mx-6 mt-auto">
                   <a
                     href="#"
@@ -253,7 +251,14 @@ export default function Dashboard({ children }: { children: any }) {
                       alt=""
                     />
                     <span className="sr-only">Your profile</span>
-                    <span aria-hidden="true">Tom Cook</span>
+
+                    <span
+                      className={`overflow-hidden transition-all ${
+                        expanded ? "w-52" : "w-0"
+                      }`}
+                    >
+                      Salym
+                    </span>
                   </a>
                 </li>
               </ul>
@@ -276,7 +281,12 @@ export default function Dashboard({ children }: { children: any }) {
           <UserNav />
         </div>
 
-        <main className="py-10 lg:pl-72">
+        <main
+          className={classNames(
+            expanded ? "lg:pl-72" : "lg:pl-20",
+            "transition-all py-10"
+          )}
+        >
           <div className="px-4 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
