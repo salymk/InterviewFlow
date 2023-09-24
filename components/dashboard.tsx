a5c050b23347a0c94df0e950e0dbd85c7426ad10
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-09-24 13:07:15 -0600

    style: update mobile menu colors to match desktop

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -109,53 +109,21 @@ export default function Dashboard({ children }: { children: any }) {
                                   href={item.href}
                                   className={classNames(
                                     item.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                      ? "bg-slate-950 text-slate-100"
+                                      : "text-slate-950 hover:text-slate-100 hover:bg-slate-950",
                                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                   )}
                                 >
                                   <item.icon
                                     className={classNames(
                                       item.current
-                                        ? "text-indigo-600"
-                                        : "text-gray-400 group-hover:text-indigo-600",
+                                        ? "text-slate-100"
+                                        : "text-slate-950 group-hover:text-slate-100",
                                       "h-6 w-6 shrink-0"
                                     )}
                                     aria-hidden="true"
                                   />
                                   {item.name}
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
-                                >
-                                  <span
-                                    className={classNames(
-                                      team.current
-                                        ? "text-indigo-600 border-indigo-600"
-                                        : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                                      "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                                    )}
-                                  >
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
                                 </a>
                               </li>
                             ))}
@@ -213,10 +181,8 @@ export default function Dashboard({ children }: { children: any }) {
                             "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
                             item.current
                               ? "bg-slate-950 text-slate-100"
-                              : "text-slate-950",
-                            expanded ? "ml-0" : "ml-1",
-                            !item.current &&
-                              "hover:text-slate-100 hover:bg-slate-950"
+                              : "text-slate-950 hover:text-slate-100 hover:bg-slate-950",
+                            expanded ? "ml-0" : "ml-1"
                           )}
                         >
                           <item.icon
